bc97e4fadc43b805c2ac2a3121c9a6cc1ffe58d6
jose luis <cortezluisjose@gmail.com>
2019-08-12 03:49:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,5 +5,12 @@ end
 def WIN_COMBINATIONS(8," ")
   WIN_COMBINATIONS = [0,1,2,5],[3,4,7],[6,8]
 end
+	 def display_board
+	 puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+	 puts "-----------"
+	 puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+	 puts "-----------"
+	 puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+	 end
 
 end
