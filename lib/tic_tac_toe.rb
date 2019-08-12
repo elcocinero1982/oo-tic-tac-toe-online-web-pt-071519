dfc6b929fd48b53d22fb34684dbf09d50c063b1b
jose luis <cortezluisjose@gmail.com>
2019-08-12 03:56:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize(board=nil)
   @board=board||Array.new(9, " ")
 end
-def WIN_COMBINATIONS
+def WIN_COMBINATIONS(8, " ")
 end
 	 def display_board
 	 puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -10,6 +10,10 @@ end
 	 puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
 	 puts "-----------"
 	 puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+	 end
+	 def input_to_index
+	   
+	   
 	 end
 
 end
